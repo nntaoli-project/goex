cfd1ae53767f4fa7d86c7563ee1afc0897ff5afd
nntaoli <2767415655@qq.com>
2016-05-27 16:31:47 +0800

    hmac sign

--- unit_test/ParamSignUtils_test.go
+++ unit_test/ParamSignUtils_test.go
@@ -12,6 +12,17 @@ func Test_appendParamSign(t *testing.T) {
 	secret := "secret";
 
 	sign, _ := rest.GetParamMD5Sign(secret, params);
-	assert.Equal(t, strings.ToUpper(sign), "E0AEBA5156E0FEE8BDBC3B3A963C7968");
+	assert.Equal(t, strings.ToUpper(sign), "E0AEBA5156E0FEE8BDBC3B3A963C7968", "md5 sign fail");
+
+	hmacSha256Sign, _ := rest.GetParamHmacSHA256Sign(secret, params);
+	assert.Equal(t, hmacSha256Sign, "c5b19a95474fc71f7e054d596452b8fe7c3ff517b5a05c12804da4ba6f987664", "hmac sha256 sign fail");
+
+	hmacSha1Sign, _ := rest.GetParamHmacSHA1Sign(secret, params);
+	assert.Equal(t, hmacSha1Sign, "1c0a1aabae2dd88e728f6d84c46656b1b07d0fce", "hmac sha1 sign fail");
+
+	hmacSha512Sign, _ := rest.GetParamHmacSHA512Sign(secret, params);
+	assert.Equal(t, hmacSha512Sign,
+		"46b7c47269ead767a7ce2d4b385cba52f0450e2ea6ffb68c11ce620453a7a3ab62e36082775c94bd6859fccba1f9819a83cc94dbfb416c7c8c510882463ce3a2",
+		"hmac sha512 sign fail");
 }
 
