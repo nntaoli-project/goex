40bd49e2966142f153dae6647d212fd648d9c521
nntaoli <2767415655@qq.com>
2017-03-13 11:27:34 +0800

    DepthDecord Implement The Sort Interface

--- Metas.go
+++ Metas.go
@@ -52,9 +52,23 @@ type DepthRecord struct {
 	Amount float64;
 }
 
+type DepthRecords []DepthRecord
+
+func (dr DepthRecords) Len() int {
+	return len(dr)
+}
+
+func (dr DepthRecords) Swap(i, j int) {
+	dr[i], dr[j] = dr[j], dr[i]
+}
+
+func (dr DepthRecords) Less(i, j int) bool {
+	return dr[i].Price < dr[j].Price
+}
+
 type Depth struct {
 	AskList,
-	BidList []DepthRecord
+	BidList DepthRecords
 }
 
 type APIConfig struct {
