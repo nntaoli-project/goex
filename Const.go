2f0b377a5624d57b8a9ef8bf53d88474b16f9110
nntaoli <2767415655@qq.com>
2017-07-07 13:19:27 +0800

    reformat (重新格式化文件排版)

--- Const.go
+++ Const.go
@@ -1,51 +1,50 @@
 package coinapi
 
-type CurrencyPair int;
+type CurrencyPair int
 
 func (c CurrencyPair) String() string {
 	if c == 0 {
 		return "nil"
 	}
-	return currencyPairSymbol[c - 1];
+	return currencyPairSymbol[c-1]
 }
 
-type Currency int;
+type Currency int
 
 func (c Currency) String() string {
 	if c == 0 {
 		return "nil"
 	}
-	return currencySymbol[c - 1];
+	return currencySymbol[c-1]
 }
 
-type TradeSide int;
+type TradeSide int
 
-func (ts TradeSide)String() string {
+func (ts TradeSide) String() string {
 	switch ts {
 	case 1:
-		return "BUY";
+		return "BUY"
 	case 2:
-		return "SELL";
+		return "SELL"
 	case 3:
-		return "BUY_MARKET";
+		return "BUY_MARKET"
 	case 4:
-		return "SELL_MARKET";
+		return "SELL_MARKET"
 	default:
-		return "UNKNOWN";
+		return "UNKNOWN"
 	}
 }
 
-type TradeStatus int;
+type TradeStatus int
 
 func (ts TradeStatus) String() string {
-	return orderStatusSymbol[ts];
+	return orderStatusSymbol[ts]
 }
 
 /*currencySymbol array*/
-var currencySymbol = [...]string{"cny", "usd", "btc", "ltc", "eth", "etc", "zec", "sc" , "bts" , "eos"};
+var currencySymbol = [...]string{"cny", "usd", "btc", "ltc", "eth", "etc", "zec", "sc", "bts", "eos"}
 
-const
-(
+const (
 	CNY = 1 + iota
 	USD
 	BTC
@@ -58,12 +57,11 @@ const
 	EOS
 )
 
-var currencyPairSymbol = [...]string{"btc_cny", "btc_usd", "btc_jpy", "fx_btc_jpy", "ltc_cny", "ltc_usd","ltc_btc", "eth_cny",
-	"eth_usd", "eth_btc", "etc_cny", "etc_usd", "etc_btc","etc_eth","xcn_btc", "sys_btc", "zec_cny", "zec_usd", "zec_btc" , "bts_cny","bts_btc" ,
-	"sc_cny" , "eos_cny"};
+var currencyPairSymbol = [...]string{"btc_cny", "btc_usd", "btc_jpy", "fx_btc_jpy", "ltc_cny", "ltc_usd", "ltc_btc", "eth_cny",
+	"eth_usd", "eth_btc", "etc_cny", "etc_usd", "etc_btc", "etc_eth", "xcn_btc", "sys_btc", "zec_cny", "zec_usd", "zec_btc", "bts_cny", "bts_btc",
+	"sc_cny", "eos_cny"}
 
-const
-(
+const (
 	BTC_CNY = 1 + iota
 	BTC_USD
 	BTC_JPY
@@ -97,8 +95,7 @@ const
 	EOS_CNY
 )
 
-const
-(
+const (
 	BUY = 1 + iota
 	SELL
 	BUY_MARKET
@@ -107,8 +104,7 @@ const
 
 var orderStatusSymbol = [...]string{"UNFINISH", "PART_FINISH", "FINISH", "CANCEL", "REJECT", "CANCEL_ING"}
 
-const
-(
+const (
 	ORDER_UNFINISH = iota
 	ORDER_PART_FINISH
 	ORDER_FINISH
@@ -117,33 +113,30 @@ const
 	ORDER_CANCEL_ING
 )
 
-const
-(
-	OPEN_BUY = 1 + iota  //开多
-	OPEN_SELL              //开空
+const (
+	OPEN_BUY   = 1 + iota //开多
+	OPEN_SELL             //开空
 	CLOSE_BUY             //平多
-	CLOSE_SELL           //平空
+	CLOSE_SELL            //平空
 )
 
 var CurrencyPairSymbol = map[CurrencyPair]string{
-	BTC_CNY : "btc_cny",
-	BTC_USD : "btc_usd",
-	LTC_CNY : "ltc_cny",
-	LTC_USD : "ltc_usd",
-	ETH_CNY : "eth_cny",
-	ETH_USD : "eth_usd",
-	ETH_BTC : "eth_btc",
-	ETC_CNY : "etc_cny",
-	ETC_USD : "etc_usd",
-	ETC_BTC : "etc_btc",
-	BTS_CNY : "bts_cny",
-	SC_CNY  : "sc_cny",
-	EOS_CNY : "eos_cny"};
+	BTC_CNY: "btc_cny",
+	BTC_USD: "btc_usd",
+	LTC_CNY: "ltc_cny",
+	LTC_USD: "ltc_usd",
+	ETH_CNY: "eth_cny",
+	ETH_USD: "eth_usd",
+	ETH_BTC: "eth_btc",
+	ETC_CNY: "etc_cny",
+	ETC_USD: "etc_usd",
+	ETC_BTC: "etc_btc",
+	BTS_CNY: "bts_cny",
+	SC_CNY:  "sc_cny",
+	EOS_CNY: "eos_cny"}
 
-var
-(
-	THIS_WEEK_CONTRACT = "this_week"; //周合约
-	NEXT_WEEK_CONTRACT = "next_week"; //次周合约
-	QUARTER_CONTRACT = "quarter"; //季度合约
+var (
+	THIS_WEEK_CONTRACT = "this_week" //周合约
+	NEXT_WEEK_CONTRACT = "next_week" //次周合约
+	QUARTER_CONTRACT   = "quarter"   //季度合约
 )
-
