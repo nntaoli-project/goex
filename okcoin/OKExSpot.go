5c841951918505b77c2458de53ff6b851761d69f
nntaoli <2767415655@qq.com>
2017-09-12 10:02:06 +0800

    [okex spot] GetAccount

--- okcoin/OKExSpot.go
+++ okcoin/OKExSpot.go
@@ -1,6 +1,13 @@
 package okcoin
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	. "github.com/nntaoli-project/GoEx"
+	"net/http"
+	"net/url"
+	"strconv"
+)
 
 type OKExSpot struct {
 	OKCoinCN_API
@@ -13,4 +20,81 @@ func NewOKExSpot(client *http.Client, accesskey, secretkey string) *OKExSpot {
 
 func (ctx *OKExSpot) GetExchangeName() string {
 	return "okex.com"
+}
+
+func (ctx *OKExSpot) GetAccount() (*Account, error) {
+	postData := url.Values{}
+	err := ctx.buildPostForm(&postData)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := HttpPostForm(ctx.client, ctx.api_base_url+url_userinfo, postData)
+	if err != nil {
+		return nil, err
+	}
+
+	var respMap map[string]interface{}
+
+	err = json.Unmarshal(body, &respMap)
+	if err != nil {
+		return nil, err
+	}
+
+	if !respMap["result"].(bool) {
+		errcode := strconv.FormatFloat(respMap["error_code"].(float64), 'f', 0, 64)
+		return nil, errors.New(errcode)
+	}
+	//log.Println(respMap)
+	info, ok := respMap["info"].(map[string]interface{})
+	if !ok {
+		return nil, errors.New(string(body))
+	}
+
+	funds := info["funds"].(map[string]interface{})
+	free := funds["free"].(map[string]interface{})
+	freezed := funds["freezed"].(map[string]interface{})
+
+	account := new(Account)
+	account.Exchange = ctx.GetExchangeName()
+
+	var btcSubAccount SubAccount
+	var ltcSubAccount SubAccount
+	var ethSubAccount SubAccount
+	var etcSubAccount SubAccount
+	var bccSubAccount SubAccount
+
+	btcSubAccount.Currency = BTC
+	btcSubAccount.Amount = ToFloat64(free["btc"])
+	btcSubAccount.LoanAmount = 0
+	btcSubAccount.ForzenAmount = ToFloat64(freezed["btc"])
+
+	ltcSubAccount.Currency = LTC
+	ltcSubAccount.Amount = ToFloat64(free["ltc"])
+	ltcSubAccount.LoanAmount = 0
+	ltcSubAccount.ForzenAmount = ToFloat64(freezed["ltc"])
+
+	ethSubAccount.Currency = ETH
+	ethSubAccount.Amount = ToFloat64(free["eth"])
+	ethSubAccount.LoanAmount = 0
+	ethSubAccount.ForzenAmount = ToFloat64(freezed["eth"])
+
+	etcSubAccount.Currency = ETC
+	etcSubAccount.Amount = ToFloat64(free["etc"])
+	etcSubAccount.LoanAmount = 0
+	etcSubAccount.ForzenAmount = ToFloat64(freezed["etc"])
+
+	bccSubAccount.Currency = BCC
+	bccSubAccount.Amount = ToFloat64(free["bcc"])
+	bccSubAccount.LoanAmount = 0
+	bccSubAccount.ForzenAmount = ToFloat64(freezed["bcc"])
+
+	account.SubAccounts = make(map[Currency]SubAccount, 5)
+	account.SubAccounts[BTC] = btcSubAccount
+	account.SubAccounts[LTC] = ltcSubAccount
+	account.SubAccounts[ETH] = ethSubAccount
+	account.SubAccounts[ETC] = etcSubAccount
+	account.SubAccounts[BCC] = bccSubAccount
+
+	return account, nil
 }
