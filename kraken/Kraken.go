e7f62df377c8a964471109a5ff1ebd491d06236f
glacier2002 <hu_zhihao@sina.cn>
2017-10-30 23:54:14 +0800

    update kraken

--- kraken/Kraken.go
+++ kraken/Kraken.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/nntaoli-project/GoEx"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 )
@@ -13,9 +14,12 @@ import (
 const (
 	EXCHANGE_NAME = "kraken.com"
 
-	API_BASE_URL = "https://api.kraken.com/0/public/Ticker"
-
-	TICKER_URI = "?pair=%s"
+	API_BASE_URL = "https://api.kraken.com"
+	API_V0       = "/0/"
+	PUBLIC       = "public/"
+	PRIVATE      = "private/"
+	TICKER_URI   = "Ticker?pair=%s"
+	ACCOUNT_URI  = "Balance"
 )
 
 type Kraken struct {
@@ -34,30 +38,31 @@ func (kraken *Kraken) GetExchangeName() string {
 
 func (kraken *Kraken) GetTicker(currency CurrencyPair) (*Ticker, error) {
 	curr := currency.ToSymbol("")
-	tickerUri := fmt.Sprintf(API_BASE_URL+TICKER_URI, curr)
-	//log.Println("Kraken tickerUri:",tickerUri)
+	tickerUri := fmt.Sprintf(API_BASE_URL+API_V0+PUBLIC+TICKER_URI, curr)
 	bodyDataMap, err := HttpGet(kraken.httpClient, tickerUri)
-	//log.Println("Kraken bodyDataMap:",bodyDataMap)
 
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	//result, isok := bodyDataMap["error"].([]interface{})
-	//log.Println("bodyDataMap[\"error\"]",result, isok)
+
 	if result, _ := bodyDataMap["error"].([]interface{}); len(result) != 0 {
 		log.Println("bodyDataMap[\"error\"]", result)
 		return nil, errors.New("err")
 	}
+	switch currency.CurrencyA.Symbol {
+	case "XBT", "XRP", "ETC", "XMR":
+		curr = "X" + currency.ToSymbol("Z")
+	}
+
 	curr = "X" + currency.ToSymbol("Z")
 	tickerMap := bodyDataMap["result"].(map[string]interface{})
-	//log.Println("Kraken curr:",curr)
-	//log.Println("Kraken tickerMap:",tickerMap)
 	var ticker Ticker
-	tickers := tickerMap[curr].(map[string]interface{})
-	//log.Println("Kraken tickers:",tickers)
+	tickers, isOk := tickerMap[curr].(map[string]interface{})
+	if isOk != true {
+		return nil, errors.New("err")
+	}
 
-	//fmt.Println(bodyDataMap)
 	timestamp := time.Now().Unix()
 	ticker.Date = uint64(timestamp)
 
@@ -76,4 +81,61 @@ func (kraken *Kraken) GetTicker(currency CurrencyPair) (*Ticker, error) {
 	log.Println("Kraken", currency, "ticker:", ticker)
 
 	return &ticker, nil
+}
+
+func (kraken *Kraken) GetDepth(size int, currencyPair CurrencyPair) (*Depth, error) {
+	panic("not implement")
+
+}
+
+func (kraken *Kraken) buildParamsSigned(method string, postForm *url.Values) string {
+	//	postForm.Set("nonce", fmt.Sprintf("%d", time.Now().UnixNano()))
+	//	urlPath := API_V0+PRIVATE+method
+	// Create signature
+	//secret, _ := base64.StdEncoding.DecodeString(api.secret)
+	//signature := createSignature(urlPath, values, secret)
+
+	return nil
+}
+func (kraken *Kraken) queryPrivate(method string, values url.Values, typ interface{}) (interface{}, error) {
+
+}
+func (kraken *Kraken) placeOrder(amount, price string, pair CurrencyPair, orderType, orderSide string) (*Order, error) {
+	panic("not implement")
+
+}
+
+func (kraken *Kraken) GetAccount() (*Account, error) {
+	panic("not implement")
+
+}
+
+func (kraken *Kraken) LimitBuy(amount, price string, currencyPair CurrencyPair) (*Order, error) {
+	panic("not implement")
+}
+
+func (kraken *Kraken) LimitSell(amount, price string, currencyPair CurrencyPair) (*Order, error) {
+	panic("not implement")
+}
+
+func (kraken *Kraken) MarketBuy(amount, price string, currencyPair CurrencyPair) (*Order, error) {
+	panic("not implement")
+}
+
+func (kraken *Kraken) MarketSell(amount, price string, currencyPair CurrencyPair) (*Order, error) {
+	panic("not implement")
+}
+
+func (kraken *Kraken) CancelOrder(orderId string, currencyPair CurrencyPair) (bool, error) {
+	panic("not implement")
+
+}
+
+func (kraken *Kraken) GetOneOrder(orderId string, currencyPair CurrencyPair) (*Order, error) {
+	panic("not implement")
+
+}
+
+func (kraken *Kraken) GetUnfinishOrders(currencyPair CurrencyPair) ([]Order, error) {
+	panic("not implement")
 }
