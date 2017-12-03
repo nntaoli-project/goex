5cbac950ba11574d4713128811c24e02690cb1eb
nntaoli <2767415655@qq.com>
2017-12-03 23:20:09 +0800

    [bittrex] public api [GetDepth, GetTicker]

---
+++ bittrex/bittrex_test.go
@@ -0,0 +1,22 @@
+package bittrex
+
+import (
+	"github.com/nntaoli-project/GoEx"
+	"net/http"
+	"testing"
+)
+
+var b = New(http.DefaultClient, "", "")
+
+func TestBittrex_GetTicker(t *testing.T) {
+	ticker, err := b.GetTicker(goex.BTC_USDT)
+	t.Log("err=>", err)
+	t.Log("ticker=>", ticker)
+}
+
+func TestBittrex_GetDepth(t *testing.T) {
+	dep, err := b.GetDepth(1, goex.BTC_USDT)
+	t.Log("err=>", err)
+	t.Log("ask=>", dep.AskList)
+	t.Log("bid=>", dep.BidList)
+}
