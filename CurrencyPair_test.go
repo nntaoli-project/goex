9b3f82d63963ac5cf07a50c200def61c0122cfe7
glacier2002 <hu_zhihao@sina.cn>
2018-04-07 21:58:16 +0800

    add a function NewCurrencyPairsFromStrings to new currency pairs from strings

--- CurrencyPair_test.go
+++ CurrencyPair_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestCurrency2_String(t *testing.T) {
 	btc := NewCurrency("btc", "bitcoin")
-	btc2 := Currency{"BTC" , "bitcoin.org"}
+	btc2 := Currency{"BTC", "bitcoin.org"}
 	ltc := NewCurrency("ltc", "litecoin")
 	t.Log(btc == BTC)
-	t.Log(ltc.Desc , btc.Desc)
+	t.Log(ltc.Desc, btc.Desc)
 	t.Log(btc == btc2)
 }
 
@@ -18,4 +18,8 @@ func TestCurrencyPair2_String(t *testing.T) {
 	btccny := NewCurrencyPair(NewCurrency("btc", ""), NewCurrency("cny", ""))
 	t.Log(strings.ToUpper(btccny.String()))
 	t.Log(BTC_CNY)
+}
+func TestFormatPairs_String(t *testing.T) {
+	p := NewCurrencyPairsFromStrings("BTC_USD,LTC_USD,LTC_BTC,ETH_USD,ETH_BTC,ETC_BTC,ETC_USD", ",")
+	t.Log(p)
 }
