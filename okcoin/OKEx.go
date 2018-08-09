80d8b0dcdb36bb4f57d7c4b1010568fbb9c61f57
dude <79403828@qq.com>
2018-08-09 18:00:53 +0800

    add future trading supports for XRP,EOS,BTG

--- okcoin/OKEx.go
+++ okcoin/OKEx.go
@@ -228,6 +228,9 @@ type futureUserInfoResponse struct {
 		Etc map[string]float64 `json:"etc"`
 		Eth map[string]float64 `json:"eth"`
 		Bch map[string]float64 `json:"bch"`
+		Xrp map[string]float64 `json:"xrp"`
+		Eos map[string]float64 `json:"eos"`
+		Btg map[string]float64 `json:"btg"`
 	} `json:info`
 	Result     bool `json:"result,bool"`
 	Error_code int  `json:"error_code"`
@@ -264,12 +267,18 @@ func (ok *OKEx) GetFutureUserinfo() (*FutureAccount, error) {
 	bchMap := resp.Info.Bch
 	ethMap := resp.Info.Eth
 	etcMap := resp.Info.Etc
+	xrpMap := resp.Info.Xrp
+	eosMap := resp.Info.Eos
+	btgMap := resp.Info.Btg
 
 	account.FutureSubAccounts[BTC] = FutureSubAccount{BTC, btcMap["account_rights"], btcMap["keep_deposit"], btcMap["profit_real"], btcMap["profit_unreal"], btcMap["risk_rate"]}
 	account.FutureSubAccounts[LTC] = FutureSubAccount{LTC, ltcMap["account_rights"], ltcMap["keep_deposit"], ltcMap["profit_real"], ltcMap["profit_unreal"], ltcMap["risk_rate"]}
 	account.FutureSubAccounts[BCH] = FutureSubAccount{BCH, bchMap["account_rights"], bchMap["keep_deposit"], bchMap["profit_real"], bchMap["profit_unreal"], bchMap["risk_rate"]}
 	account.FutureSubAccounts[ETH] = FutureSubAccount{ETH, ethMap["account_rights"], ethMap["keep_deposit"], ethMap["profit_real"], ethMap["profit_unreal"], ethMap["risk_rate"]}
 	account.FutureSubAccounts[ETC] = FutureSubAccount{ETC, etcMap["account_rights"], etcMap["keep_deposit"], etcMap["profit_real"], etcMap["profit_unreal"], etcMap["risk_rate"]}
+	account.FutureSubAccounts[XRP] = FutureSubAccount{XRP, xrpMap["account_rights"], xrpMap["keep_deposit"], xrpMap["profit_real"], xrpMap["profit_unreal"], xrpMap["risk_rate"]}
+	account.FutureSubAccounts[EOS] = FutureSubAccount{EOS, eosMap["account_rights"], eosMap["keep_deposit"], eosMap["profit_real"], eosMap["profit_unreal"], eosMap["risk_rate"]}
+	account.FutureSubAccounts[BTG] = FutureSubAccount{BTG, btgMap["account_rights"], btgMap["keep_deposit"], btgMap["profit_real"], btgMap["profit_unreal"], btgMap["risk_rate"]}
 
 	return account, nil
 }
