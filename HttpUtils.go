b4dc88099d99645b0260c3418819a1ce60c08006
lichongbo <55309107@qq.com>
2018-10-19 10:35:43 +0800

    降低内存泄露的可能

--- HttpUtils.go
+++ HttpUtils.go
@@ -13,7 +13,10 @@ import (
 )
 
 func NewHttpRequest(client *http.Client, reqType string, reqUrl string, postData string, requstHeaders map[string]string) ([]byte, error) {
-	req, _ := http.NewRequest(reqType, reqUrl, strings.NewReader(postData))
+	req, err := http.NewRequest(reqType, reqUrl, strings.NewReader(postData))
+	if err != nil {
+		return nil, err
+	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36")
 
 	if requstHeaders != nil {
@@ -23,11 +26,12 @@ func NewHttpRequest(client *http.Client, reqType string, reqUrl string, postData
 	}
 
 	resp, err := client.Do(req)
+	if(resp != nil){
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return nil, err
 	}
-
-	defer resp.Body.Close()
 
 	bodyData, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
