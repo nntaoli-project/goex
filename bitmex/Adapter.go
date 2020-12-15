8b6aa46acf273557cbc4f982926bdab059a50382
nntaoli <2767415655@qq.com>
2020-12-15 16:19:46 +0800

    [bitmes] ws implement

---
+++ bitmex/Adapter.go
@@ -0,0 +1,57 @@
+package bitmex
+
+import (
+	"fmt"
+	. "github.com/nntaoli-project/goex"
+	"strings"
+)
+
+func AdaptCurrencyPairToSymbol(pair CurrencyPair, contract string) string {
+	if contract == "" || contract == SWAP_CONTRACT {
+		if pair.CurrencyA.Eq(BTC) {
+			pair = NewCurrencyPair(XBT, USD)
+		}
+		if pair.CurrencyB.Eq(BTC) {
+			pair = NewCurrencyPair(pair.CurrencyA, XBT)
+		}
+		return pair.AdaptUsdtToUsd().ToSymbol("")
+	}
+
+	coin := pair.CurrencyA.Symbol
+	if pair.CurrencyA.Eq(BTC) {
+		coin = XBT.Symbol
+	}
+	return fmt.Sprintf("%s%s", coin, strings.ToUpper(contract))
+}
+
+func AdaptWsSymbol(symbol string) (pair CurrencyPair, contract string) {
+	symbol = strings.ToUpper(symbol)
+
+	if symbol == "XBTCUSD" {
+		return BTC_USD, SWAP_CONTRACT
+	}
+
+	if symbol == "BCHUSD" {
+		return BCH_USD, SWAP_CONTRACT
+	}
+
+	if symbol == "ETHUSD" {
+		return ETH_USD, SWAP_CONTRACT
+	}
+
+	if symbol == "LTCUSD" {
+		return LTC_USD, SWAP_CONTRACT
+	}
+
+	if symbol == "LINKUSDT" {
+		return NewCurrencyPair2("LINK_USDT"), SWAP_CONTRACT
+	}
+
+	pair = NewCurrencyPair(NewCurrency(symbol[0:3], ""), USDT)
+	contract = symbol[3:]
+	if pair.CurrencyA.Eq(XBT) {
+		return NewCurrencyPair(BTC, USDT), contract
+	}
+
+	return pair, contract
+}
