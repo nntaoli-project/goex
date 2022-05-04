6d163df05a388019c6503c4ab70a142be2a531ef
WymA <mathews2wym@gmail.com>
2022-05-04 13:04:08 +0800

    1) add testnet for binance, 2) add binance futures subscribe depth example

---
+++ examples/binance_futures_trade.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/nntaoli-project/goex"
+	"github.com/nntaoli-project/goex/binance"
+	"github.com/nntaoli-project/goex/builder"
+)
+
+const (
+	BINANCE_TESTNET_API_KEY        = "***REMOVED***"
+	BINANCE_TESTNET_API_KEY_SECRET = "***REMOVED***"
+)
+
+func main() {
+
+	api, err := builder.DefaultAPIBuilder.APIKey(BINANCE_TESTNET_API_KEY).APISecretkey(BINANCE_TESTNET_API_KEY_SECRET).Endpoint(binance.TESTNET_FUTURE_USD_WS_BASE_URL).BuildFuturesWs(goex.BINANCE_FUTURES)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	//api.SubscribeTicker(goex.BTC_USD, goex.SWAP_USDT_CONTRACT)
+	api.DepthCallback(func(depth *goex.Depth) {
+		log.Println(depth)
+	})
+	api.SubscribeDepth(goex.BTC_USDT, goex.SWAP_USDT_CONTRACT)
+
+	time.Sleep(time.Minute) // run for one minute
+
+}
