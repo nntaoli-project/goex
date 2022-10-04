6a9f6862849c2f95bac45d984ba320d9959071a6
nntaoli <2767415655@qq.com>
2022-10-04 18:33:16 +0800

    ticker,depth,kline model

--- models.go
+++ models.go
@@ -1,7 +1,12 @@
 package goex
 
+import (
+	"time"
+)
+
 type CurrencyPair struct {
 	Symbol         string
+	Market         string
 	PricePrecision int //价格小数点位数
 	QtyPrecision   int //数量小数点位数
 	MinQty         float64
@@ -14,4 +19,53 @@ type FuturesCurrencyPair struct {
 	DeliveryDate int64   //结算日期
 	OnboardDate  int64   //上线日期
 	MarginAsset  float64 //保证金资产
+}
+
+type Ticker struct {
+	Pair     CurrencyPair `json:"pair"`
+	Last     float64      `json:"l"`
+	Buy      float64      `json:"b"`
+	Sell     float64      `json:"s"`
+	High     float64      `json:"h"`
+	Low      float64      `json:"lw"`
+	Vol      float64      `json:"v"`
+	DateTime uint64       `json:"t"`
+	Origin   []byte       `json:"origin"`
+}
+
+type DepthItem struct {
+	Price  float64 `json:"price"`
+	Amount float64 `json:"amount"`
+}
+
+type DepthItems []DepthItem
+
+func (dr DepthItems) Len() int {
+	return len(dr)
+}
+
+func (dr DepthItems) Swap(i, j int) {
+	dr[i], dr[j] = dr[j], dr[i]
+}
+
+func (dr DepthItems) Less(i, j int) bool {
+	return dr[i].Price < dr[j].Price
+}
+
+type Depth struct {
+	Pair   CurrencyPair `json:"pair"`
+	UTime  time.Time    `json:"ut"`
+	Asks   DepthItems   `json:"asks"`
+	Bids   DepthItems   `json:"bids"`
+	Origin []byte       `json:"origin"`
+}
+
+type Kline struct {
+	Pair      CurrencyPair `json:"pair"`
+	Timestamp int64        `json:"t"`
+	Open      float64      `json:"o"`
+	Close     float64      `json:"s"`
+	High      float64      `json:"h"`
+	Low       float64      `json:"l"`
+	Vol       float64      `json:"v"`
 }
