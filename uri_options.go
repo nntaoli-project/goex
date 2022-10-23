114459be625bd9ac8eef492ea144bf7641a1a5a5
nntaoli <2767415655@qq.com>
2022-10-23 22:38:30 +0800

    urioptions和apioptions配置

---
+++ uri_options.go
@@ -0,0 +1,62 @@
+package goex
+
+type UriOptions struct {
+	Endpoint            string
+	TickerUri           string
+	DepthUri            string
+	KlineUri            string
+	GetOrderUri         string
+	GetPendingOrdersUri string
+	CancelOrderUri      string
+	NewOrderUri         string
+}
+
+type UriOption func(*UriOptions)
+
+func WithEndpoint(endpoint string) UriOption {
+	return func(c *UriOptions) {
+		c.Endpoint = endpoint
+	}
+}
+
+func WithTickerUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.TickerUri = uri
+	}
+}
+
+func WithDepthUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.DepthUri = uri
+	}
+}
+
+func WithKlineUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.KlineUri = uri
+	}
+}
+
+func WithGetOrderUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.GetOrderUri = uri
+	}
+}
+
+func WithGetPendingOrdersUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.GetPendingOrdersUri = uri
+	}
+}
+
+func WithCancelOrderUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.CancelOrderUri = uri
+	}
+}
+
+func WithNewOrderUri(uri string) UriOption {
+	return func(c *UriOptions) {
+		c.NewOrderUri = uri
+	}
+}
