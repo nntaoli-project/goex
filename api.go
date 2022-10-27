3d64d615dde6ab336e0ebb65fdd9d03ea879f2be
nntaoli <2767415655@qq.com>
2022-10-27 13:56:44 +0800

    [api] ITradeRest Add DoAuthRequest Method

--- api.go
+++ api.go
@@ -8,10 +8,6 @@ type IHttpClient interface {
 	DoRequest(method, rqUrl string, params *url.Values, headers map[string]string) (data []byte, err error)
 }
 
-// IProtocolParser 协议解析器
-type IProtocolParser interface {
-}
-
 // IMarketRest 行情接口，不需要授权
 type IMarketRest interface {
 	GetName() string //获取交易所名字/域名
@@ -23,12 +19,13 @@ type IMarketRest interface {
 // ITradeRest 交易相关的接口
 type ITradeRest interface {
 	CreateOrder(order Order, opt ...OptionParameter) (*Order, error) //创建订单
-	// GetOrderInfo 批量创建订单,考虑中，是否有意义
+	//CreateOrders 批量创建订单,考虑中，是否有意义
 	//CreateOrders(orders []Order, opt ...OptionParameter) ([]Order, error)
 	GetOrderInfo(pair CurrencyPair, id string, opt ...OptionParameter) (*Order, error)
 	GetPendingOrders(pair CurrencyPair, opt ...OptionParameter) ([]Order, error)
 	CancelOrder(pair CurrencyPair, id string, opt ...OptionParameter) error
-	CancelOrders(pair *CurrencyPair, id []string, opt ...OptionParameter) error
+	//CancelOrders(pair *CurrencyPair, id []string, opt ...OptionParameter) error
+	DoAuthRequest(method, reqUrl string, params *url.Values, header map[string]string) ([]byte, error)
 }
 
 // IWallet 获取资产信息，划转资金等操作
