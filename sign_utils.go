a4c17cb93b847314db9ebb966d5d3bb5887a6bfc
nntaoli <2767415655@qq.com>
2022-10-27 11:05:05 +0800

    sign utils

--- sign_utils.go
+++ sign_utils.go
@@ -10,10 +10,7 @@ import (
 	"encoding/hex"
 )
 
-/**
- *md5签名,okcoin和huobi适用
- */
-func GetParamMD5Sign(secret, params string) (string, error) {
+func MD5Sign(secret, params string) (string, error) {
 	hash := md5.New()
 	_, err := hash.Write([]byte(params))
 
@@ -24,16 +21,7 @@ func GetParamMD5Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
-func GetSHA(text string) (string, error) {
-	sha := sha1.New()
-	_, err := sha.Write([]byte(text))
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(sha.Sum(nil)), nil
-}
-
-func GetParamHmacSHA256Sign(secret, params string) (string, error) {
+func HmacSHA256Sign(secret, params string) (string, error) {
 	mac := hmac.New(sha256.New, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -42,7 +30,7 @@ func GetParamHmacSHA256Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(mac.Sum(nil)), nil
 }
 
-func GetParamHmacSHA512Sign(secret, params string) (string, error) {
+func HmacSHA512Sign(secret, params string) (string, error) {
 	mac := hmac.New(sha512.New, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -51,7 +39,7 @@ func GetParamHmacSHA512Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(mac.Sum(nil)), nil
 }
 
-func GetParamHmacSHA1Sign(secret, params string) (string, error) {
+func HmacSHA1Sign(secret, params string) (string, error) {
 	mac := hmac.New(sha1.New, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -60,7 +48,7 @@ func GetParamHmacSHA1Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(mac.Sum(nil)), nil
 }
 
-func GetParamHmacMD5Sign(secret, params string) (string, error) {
+func HmacMD5Sign(secret, params string) (string, error) {
 	mac := hmac.New(md5.New, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -69,8 +57,7 @@ func GetParamHmacMD5Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(mac.Sum(nil)), nil
 }
 
-/*for bitfinex.com*/
-func GetParamHmacSha384Sign(secret, params string) (string, error) {
+func HmacSha384Sign(secret, params string) (string, error) {
 	mac := hmac.New(sha512.New384, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -79,7 +66,7 @@ func GetParamHmacSha384Sign(secret, params string) (string, error) {
 	return hex.EncodeToString(mac.Sum(nil)), nil
 }
 
-func GetParamHmacSHA256Base64Sign(secret, params string) (string, error) {
+func HmacSHA256Base64Sign(secret, params string) (string, error) {
 	mac := hmac.New(sha256.New, []byte(secret))
 	_, err := mac.Write([]byte(params))
 	if err != nil {
@@ -89,13 +76,13 @@ func GetParamHmacSHA256Base64Sign(secret, params string) (string, error) {
 	return base64.StdEncoding.EncodeToString(signByte), nil
 }
 
-func GetParamHmacSHA512Base64Sign(hmac_key string, hmac_data string) string {
-	hmh := hmac.New(sha512.New, []byte(hmac_key))
-	hmh.Write([]byte(hmac_data))
+func HmacSHA512Base64Sign(hmacKey string, data string) string {
+	hmh := hmac.New(sha512.New, []byte(hmacKey))
+	hmh.Write([]byte(data))
 
-	hex_data := hex.EncodeToString(hmh.Sum(nil))
-	hash_hmac_bytes := []byte(hex_data)
+	hexData := hex.EncodeToString(hmh.Sum(nil))
+	hashHmacBytes := []byte(hexData)
 	hmh.Reset()
 
-	return base64.StdEncoding.EncodeToString(hash_hmac_bytes)
+	return base64.StdEncoding.EncodeToString(hashHmacBytes)
 }
