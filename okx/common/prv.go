6b1d737c4b35cfce4e0d9d36bb30e9a4e6208581
nntaoli <2767415655@qq.com>
2023-03-30 16:10:04 +0800

    remove log

--- okx/common/prv.go
+++ okx/common/prv.go
@@ -8,7 +8,6 @@ import (
 	"github.com/nntaoli-project/goex/v2/model"
 	"github.com/nntaoli-project/goex/v2/options"
 	"github.com/nntaoli-project/goex/v2/util"
-	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -118,7 +117,6 @@ func (prv *Prv) CancelOrder(pair model.CurrencyPair, id string, opt ...model.Opt
 func (prv *Prv) DoSignParam(httpMethod, apiUri, apiSecret, reqBody string) (signStr, timestamp string) {
 	timestamp = time.Now().UTC().Format("2006-01-02T15:04:05.000Z") //iso time style
 	payload := fmt.Sprintf("%s%s%s%s", timestamp, strings.ToUpper(httpMethod), apiUri, reqBody)
-	log.Println("payload=", payload)
 	signStr, _ = util.HmacSHA256Base64Sign(apiSecret, payload)
 	return
 }
